9cb225ca610e1dc00fba62126119d5c2abb6b37e
baldwisj <baldwisj@gmail.com>
2023-11-05 12:00:30 -0500

    added comments

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
+//All code in a Jquery function so I dont have to put a $ in front of eveything 
 $(function () {
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
+  //Variable containing all timeblocks container
   const calendarContainer = $('#calendar');
-  
+  //creating a variable with just the current hour as a string
   const currentHour = dayjs().format('H')
+  //taking the previous variable and converting it into a number
   const numHour = Number(currentHour)
+
   const timeBlock = $(".time-block")
   const todaysDate = $("#todaysDate")
+  //setting the date at the top of the page
   const today = dayjs().format('MMMM DD, YYYY');
   todaysDate.text(today);
 
+
+  //The following function loops through each elemnt in the time-block class so it can be compared to the time
   timeBlock.each(function() {
         // console.log(timeBlock)
         // console.log(Number(this.id))
         // console.log(currentHour)
-        let timeId = parseInt($(this).attr("id"));
+        let timeId = parseInt($(this).attr("id"));//The id name of each block is converted into a number
+        //The following compares the id of each block to the current hour
         if(timeId === numHour){
           $(this).addClass('present');
           $(this).removeClass('past');
@@ -50,20 +34,20 @@ $(function () {
           $(this).removeClass('present');
           $(this).removeClass('past');
         }
-
+        //Theg following code takes the text from the Local Storage and displays it in the text area
         let localText = localStorage.getItem(timeId)
         if (localText) {
           $(this).children("textarea").val(localText);
         }
   });
 
-
+  //Teh following creates an event for each button in the container and assigns a tag corresponding to each blcok
   $(calendarContainer).on("click", ".saveBtn", function(event) {
     event.preventDefault();
     let eventId = $(this).data("event")
     let eventText = $(this).siblings("textarea").val();
 
-    localStorage.setItem(eventId, (eventText));
+    localStorage.setItem(eventId, (eventText));//text and event tag is set to local storage
 
     })
     
